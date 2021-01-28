9ead239643e0f2b56e15108787e6f71a59e82a1d
Roy Hersh <royhersh@post.bgu.ac.il>
2021-01-28 15:15:01 +0200

    fix timeout + tidy + docs

--- server.py
+++ server.py
@@ -14,26 +14,46 @@ TIMEOUT = 5
 
 
 def serve(server):
-    print("listening")
-    print(select.select([server],[],[]))
-    conn, addr = server.accept()
+    """
+    to serve we will wait with select instead of using other blocking methods
+    :server: tcp socket object
+    """
+    rlist,_,_ = select.select([server],[],[])
+    conn,_ = server.accept()
     with conn:
-        print("Connected by", addr)
-        print(select.select([conn],[],[],TIMEOUT))
-        data = conn.recv(1024)
-        if data and data.decode() == PING:
-            conn.sendall(PONG.encode())
+        rlist,_,_ = select.select([conn],[],[],TIMEOUT)
+        if not rlist:#time out
+            return
+        data = conn.recv(128)
+        data = data.decode().strip()
+        if data == PING:
+            conn.send(PONG.encode())
 
 
-def main():
+def open_server(host, port):
+    """
+    The function opens the server on these interface and port
+    :host:
+    :port:
+    """
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        server.bind((HOST, PORT))
+        server.bind((host, port))
         server.listen(1)
+        print("listening at", (host, port))
         while True:
-            serve(server)
+            try:
+                serve(server)
+            except ConnectionResetError:
+                pass
+            except KeyboardInterrupt:
+                break
     finally:
+        print("Quiting")
         server.close()
+
+def main():
+    open_server(HOST, PORT)
 
 if __name__ == "__main__":
     main()
