36b675d466d80c13fb01151d658677c0536bbbb7
Roy Hersh <royhersh@post.bgu.ac.il>
2021-01-29 03:02:39 +0200

    change functions to object + support big select

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 :Purpose: ping pong TCP select server
 :Author: Roy h
 :Changes:
+28.1.20 change use in select from timeout source to change tracker for small group of sockets
 28.1.20 created
 """
 import socket
@@ -11,83 +12,97 @@ HOST = "0.0.0.0"
 PORT = 1337
 PING = "ping"
 PONG = "pong"
+MAX_SELECT_LISTEN_GROUPS = 500
 TIMEOUT = 5
+SELECT_ITER_TIMEOUT = 0.5
 
 
-def get_timeouts(conn_timeouts:dict, cur_time:int)->list:
+class SelectServer():
     """
-    :conn_timeouts: the last ping to disconnect them!
-    :cur_time:
-    :return: timeout connections
-    """
-    to_remove = []
-    for conn, conn_time in conn_timeouts.items():
-        if conn_time - cur_time > TIMEOUT:
-            to_remove.append(conn)
-    return to_remove
-
-
-def serve(server, listen_list:list, conn_timeouts:dict):
-    """
-    to serve we will wait with select instead of using other blocking methods
+    Select server object to manage shared items
     :server: tcp socket object
     :listen_list: the list to track changes
-    :conn_timeouts: the last ping to disconnect them!
-    """    
-    rlist,_,_ = select.select(listen_list,[],[], TIMEOUT)
-    
-    cur_time = time.time()
-    to_remove = get_timeouts(conn_timeouts, cur_time)
-    if rlist == []:#timeout all connections
-        to_remove = listen_list
-    else:
+    :conn_timeouts: the last ping time, to disconnect them on timeout!
+    """
+    def __init__(self, host:str, port:int) -> None:
+        """
+        :host:
+        :port:
+        """        
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.port = port
+        self.listen_list = []
+        self.conn_timeouts = {}
+
+    def get_timeouts(self, cur_time:int)->list:
+        """
+        :cur_time:
+        :return: list of timed-out connections
+        """
+        to_remove = []
+        for conn, conn_time in self.conn_timeouts.items():
+            if cur_time - conn_time > TIMEOUT:
+                to_remove.append(conn)
+        return to_remove
+
+
+    def serve(self, listen_group):
+        """
+        to serve we will wait with select instead of using other blocking methods
+        :listen_group: the current group to track changes
+        """
+        listen_group.append(self.server)
+        rlist,_,_ = select.select(listen_group,[],[], SELECT_ITER_TIMEOUT)
+        
+        cur_time = time.time()
+        to_remove = self.get_timeouts(cur_time)
         for conn in rlist:
-            if conn == server:
-                conn,_ = server.accept()
-                conn_timeouts[conn] = time.time()
-                listen_list.append(conn)
+            if conn == self.server:
+                newconn,_ = self.server.accept()
+                self.conn_timeouts[newconn] = time.time()
+                self.listen_list.append(newconn)
             elif not conn in to_remove:
                 data = conn.recv(128)
                 data = data.decode().strip()
                 if data == PING:
                     conn.send(PONG.encode())
-                    conn_timeouts[conn] = cur_time
+                    self.conn_timeouts[conn] = cur_time
                 else:
                     to_remove.append(conn)
-            
-    for conn in to_remove:
-        if conn != server:
-            conn.close()
-            listen_list.remove(conn)
-            del conn_timeouts[conn]
+                
+        for conn in to_remove:
+            if conn != self.server:
+                conn.close()
+                self.listen_list.remove(conn)
+                del self.conn_timeouts[conn]
 
+    def start(self):
+        """
+        The function opens the server on the object interface and port
+        """
+        try:
+            self.server.bind((self.host, self.port))
+            self.server.listen(0)
+            print("listening at", (self.host, self.port))
+            while True:
+                try:#MAX_SELECT_LISTEN_GROUPS
+                    for group in range(0, len(self.listen_list)+1, MAX_SELECT_LISTEN_GROUPS):
+                        self.serve(self.listen_list[group:group+MAX_SELECT_LISTEN_GROUPS])
+                except ConnectionResetError:
+                    pass
+                except KeyboardInterrupt:
+                    break
+        finally:
+            print("Quiting")
+            self.server.close()
+            self.listen_list.clear()
+            self.conn_timeouts.clear()
 
-def open_server(host:str, port:int):
-    """
-    The function opens the server on these interface and port
-    :host:
-    :port:
-    """
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        server.bind((host, port))
-        server.listen()
-        print("listening at", (host, port))
-        listen_list = [server]
-        conn_timeouts = {}
-        while True:
-            try:
-                serve(server, listen_list, conn_timeouts)
-            except ConnectionResetError:
-                pass
-            except KeyboardInterrupt:
-                break
-    finally:
-        print("Quiting")
-        server.close()
 
 def main():
-    open_server(HOST, PORT)
+    server = SelectServer(HOST, PORT)
+    server.start()
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,11 +1,19 @@
+"""
+:Purpose: load test on the ping pong server
+:Author: Roy h
+:Changes:
+28.1.20 created
+"""
 import socket, time
 
 sockets = [socket.socket() for _ in range(2000)]
 count = 0
 try:
     for sock in sockets:
+        print(f"{count}", end="\r")
         count += 1
         sock.connect(("localhost", 1337))
+    print("sleep", end="\r")
     time.sleep(5)
 except ConnectionRefusedError:
     print(count)
